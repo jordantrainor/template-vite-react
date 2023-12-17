d793679241bbb62bb3bfb3b3a3d01d9caa42c9ad
jordantrainor <jordan.g.trainor@gmail.com>
2023-12-17 04:49:49 +0000

    configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,11 +9,63 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "simple-import-sort", "unused-imports", "import"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-newline": "warn",
+    "react/jsx-curly-brace-presence": [
+      "warn",
+      {
+        props: "always",
+        children: "never",
+      },
+    ],
+    "react/jsx-sort-props": [
+      "warn",
+      {
+        callbacksLast: true,
+        multiline: "last",
+        noSortAlphabetically: false,
+        reservedFirst: true,
+        locale: "auto",
+      },
+    ],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "unused-imports/no-unused-imports": "error",
   },
+  overrides: [
+    // override "simple-import-sort" config
+    {
+      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+      rules: {
+        "simple-import-sort/imports": [
+          "error",
+          {
+            groups: [
+              // Packages `react` related packages come first.
+              ["^react", "^@?\\w"],
+              // Internal packages.
+              ["^(@|components)(/.*|$)"],
+              // Side effect imports.
+              ["^\\u0000"],
+              // Parent imports. Put `..` last.
+              ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+              // Style imports.
+              ["^.+\\.?(css)$"],
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };
